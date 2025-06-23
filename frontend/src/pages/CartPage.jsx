39fc533410f46a651ce6d6dc541e8c2a4cf59918
Shivam21512 <ugaleshivam73@gmail.com>
2025-06-23 22:20:04 +0530

    Product Card update

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { useCartStore } from '../stores/useCartStore'
+import { Link } from "react-router-dom";
+import { useCartStore } from "../stores/useCartStore";
 import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
-import { Link } from "react-router-dom";
-
-import PeopleAlsoBought from "../components/PeopleAlsoBought";
 import CartItem from "../components/CartItem";
+import PeopleAlsoBought from "../components/PeopleAlsoBought";
+import OrderSummary from "../components/OrderSummary";
+import GiftCouponCard from "../components/GiftCouponCard";
 
 const CartPage = () => {
-    const { cart } = useCartStore();
+	const { cart } = useCartStore();
 
-  return (
-
-    <div className='py-8 md:py-16'>
-        <div  className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
-            <div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
-                <motion.div
+	return (
+		<div className='py-8 md:py-16'>
+			<div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
+				<div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
+					<motion.div
 						className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'
 						initial={{ opacity: 0, x: -20 }}
 						animate={{ opacity: 1, x: 0 }}
@@ -33,15 +32,22 @@ const CartPage = () => {
 						{cart.length > 0 && <PeopleAlsoBought />}
 					</motion.div>
 
-            </div>
-
-        </div>
-
-    </div>
-     
-)
-}
-
+					{cart.length > 0 && (
+						<motion.div
+							className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
+							initial={{ opacity: 0, x: 20 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.5, delay: 0.4 }}
+						>
+							<OrderSummary />
+							<GiftCouponCard />
+						</motion.div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 export default CartPage;
 
 const EmptyCartUI = () => (
